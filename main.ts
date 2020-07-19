1ec22f303a7c7c0b9b26e5ef335b064791e6b0ac
phoenixvane <32545460+phoenixvane@users.noreply.github.com>
2020-07-20 03:47:42 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     はかる = 1
     while (はかる == 1) {
-        serial.writeValue("Celsius", input.temperature())
+        おんど = input.temperature()
+        basic.showString("" + (おんど))
+        serial.writeValue("Celsius", おんど)
         basic.pause(1000)
     }
 })
 input.onButtonPressed(Button.B, function () {
     はかる = 0
 })
+let おんど = 0
 let はかる = 0
 basic.showString("Censor")
 basic.forever(function () {
